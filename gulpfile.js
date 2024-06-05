35e03ce55153b9d07c51d242585fbc0b01707781
Jormungandr <chris-bradwell@live.co.uk>
2024-06-05 10:45:06 +0100

    Updating Mobile & Tablet code

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 import { dest, series, src, watch } from 'gulp';
-// import babel from 'gulp-babel';
+import babel from 'gulp-babel';
 
 import gulpSass from 'gulp-sass';
 import * as sass from 'sass';
 const scss = gulpSass(sass);
 
-// import autoprefixer from 'gulp-autoprefixer';
+import autoprefixer from 'gulp-autoprefixer';
 import cssMinfy from 'gulp-clean-css';
 
 import concat from 'gulp-concat';
@@ -14,38 +14,39 @@ import rename from 'gulp-rename';
 // CSS Gulp
 
 function styles() {
-  return (
-    src('./public/Jormungandr/styles/**/*.scss')
-      .pipe(scss())
-      // .pipe(autoprefixer('last 2 versions'))
-      .pipe(dest('./public/css/'))
-      .pipe(cssMinfy())
-      .pipe(
-        rename({
-          suffix: '.min',
-        })
-      )
-      .pipe(dest('./public/css/'))
-  );
+  return src('./public/Jormungandr/styles/**/*.scss')
+    .pipe(scss())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions', '> 1%'], // Adjust the browser support according to your needs
+        cascade: false,
+      })
+    )
+    .pipe(dest('./public/css/'))
+    .pipe(cssMinfy())
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(dest('./public/css/'));
 }
 
 // JS Gulp
 import jsMinfy from 'gulp-terser';
 
 function scripts() {
-  return (
-    src('./public/Jormungandr/scripts/**/*.js')
-      // .pipe(babel())
-      .pipe(concat('scripts.js'))
-      .pipe(dest('./public/js'))
-      .pipe(jsMinfy())
-      .pipe(
-        rename({
-          suffix: '.min',
-        })
-      )
-      .pipe(dest('./public/js'))
-  );
+  return src('./public/Jormungandr/scripts/**/*.js')
+    .pipe(babel())
+    .pipe(concat('scripts.js'))
+    .pipe(dest('./public/js'))
+    .pipe(jsMinfy())
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(dest('./public/js'));
 }
 
 // Watch Gulp
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,22 @@
+"use strict";
+
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-nav');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+navbarLinksContainer.addEventListener('click', e => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
+"use strict";
+
 function check() {
   var checkBox = document.getElementById('checkbox');
   var monthly = document.getElementsByClassName('monthly');
   var yearly = document.getElementsByClassName('yearly');
-
   for (var i = 0; i < monthly.length; i++) {
     if (checkBox.checked == false) {
       monthly[i].style.display = 'block';
